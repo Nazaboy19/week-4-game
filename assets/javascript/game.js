5c5a3f422f0d6935008296df502aaf30bf1b236e
Ibrahim Mercado <Nazaboy19@Ibrahims-MacBook-Pro.local>
2016-04-30 09:31:13 -0400

    prefinal commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,17 @@
-game.js
+
+
+$( "#crystal1" ).click(function() {
+  console.log ("Clicked crystal 1." );
+});
+
+$( "#crystal2" ).click(function() {
+  console.log ("Clicked crystal 2." );
+});
+
+$( "#crystal3" ).click(function() {
+  console.log ("Clicked crystal 3." );
+});
+
+$( "#crystal4" ).click(function() {
+  console.log ("Clicked crystal 4." );
+});
